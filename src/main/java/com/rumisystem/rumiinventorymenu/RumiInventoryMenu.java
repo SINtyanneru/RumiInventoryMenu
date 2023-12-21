b4e67a6ab689cf8f3f9e736c9764f489e982339b
SINtyanneru <irjitgd@gmail.com>
2023-12-21 18:46:43 +0900

    作り直すのだ2

---
+++ src/main/java/com/rumisystem/rumiinventorymenu/RumiInventoryMenu.java
@@ -0,0 +1,17 @@
+package com.rumisystem.rumiinventorymenu;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class RumiInventoryMenu extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
